9bc70a57ed2f960f80847112d272ad5e5b275a1a
kientranutc <kienkienutc95@gmail.com>
2016-12-15 15:10:22 +0700

    sua error

--- news/loi.404.php
+++ news/loi.404.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
 </head>
 <body>
- <h2>Lỗi Trang</h2>
+ <h2>Lỗi Trang nay</h2>
 	
 </body>
 </html>
